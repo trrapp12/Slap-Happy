5c66a4df5796322311bb8db98a29091b80b7514e
trrapp@gmail.com <trrapp@gmail.com>
2016-10-10 07:55:18 -0600

    chore(config) Add function to start game
    and add function to take players guess

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,28 @@
-alert ("HI")
+// psuedo coding
+//
+
+
+function introduction () {
+
+    var promptAnswer = prompt ("Do you want to play")
+
+    if (promptAnswer === "Yes" || promptAnswer === "yes") {
+      alert ("You are about to play HangMan.  Enter your guess below in the box.");
+    }
+
+    else {
+      alert ("Goodbye");
+    }
+}
+
+introduction ()
+// function myFunction() {
+//     var person = prompt("Please enter your name", "Harry Potter");
+//     if (person != null) {
+//         document.getElementById("demo").innerHTML =
+//         "Hello " + person + "! How are you today?";
+//     }
+// }
 
 function playGame () {
   var result = prompt('Do you want to play a game 2?');
@@ -12,3 +36,15 @@ function playGame () {
 }
 
 console.log(playGame(result))
+
+function getGuess () {
+  var guessVar = document.getElementById('guess').value
+
+  if (guessVar.length === 0) {
+    alert('Please enter a real value into the text box');
+
+  }
+  else {
+    alert ("You guessed " + guessVar);
+  }
+}
